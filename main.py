c8ab558c3e3f52d156800e0056173b79c3bd9d03
Darren Haz <darrenhazan@gmail.com>
2021-06-08 08:53:48 +0100

    adding functionality to read and write json file to store data

--- main.py
+++ main.py
@@ -2,53 +2,65 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator
+# Password Generator
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 def generate_password():
-
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols =[choice(symbols) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_list = password_numbers+password_letters+password_symbols
+    password_list = password_numbers + password_letters + password_symbols
     shuffle(password_list)
     password = "".join(password_list)
 
-    password_entry.delete(0,END)
-    password_entry.insert(0,password)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def on_add_click():
+def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showwarning("Oops!", "Please don't leave any fields empty!")
-    elif len(password)<8:
-        messagebox.showwarning("Password Too Short","Sorry your password is too short.\nIt must be at "
-                                                    "least 8 characters")
     else:
-        is_ok = messagebox.askokcancel("Save Password?", f"These are the details entered:\n"
-                                                         f"Website: {website}\nEmail: {email}\n"
-                                                         f"Password: {password}\n"
-                                                         f"Is it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json","w") as data_file:
+                json.dump(new_data,data_file,indent=4)
+        else:
+            #Updating old data with new
+            data.update(new_data)
 
-        if is_ok:
-            with open("saved_passwords.txt", mode="a") as password_data:
-                password_data.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated file
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -83,7 +95,7 @@ password_entry.grid(row=3, column=1, sticky="EW")
 # Buttons
 password_button = Button(text="Generate Password", bg="white", command=generate_password)
 password_button.grid(row=3, column=2, sticky="EW", padx=(10, 0))
-add_button = Button(text="Add", bg="white", command=on_add_click)
+add_button = Button(text="Add", bg="white", command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
